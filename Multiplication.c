0e6521feca9202fc75c922db616cffc078cbca91
aakanksha-m20 <91653026+aakanksha-m20@users.noreply.github.com>
2022-11-12 21:05:29 +0530

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,17 @@
+// Multiplication of two numbers
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b,Mult;
+	
+	printf("Enter values:");
+	scanf("%d%d",&a,&b);
+	
+	Mult = a * b;
+	
+	printf("Multiplication is:%d\n",Mult);
+	
+	return 0;
+}
