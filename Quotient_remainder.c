ce9b8a307b66c41725d9bfb7f7abaa38ea222435
Aakanksha Malwade <91653026+aakanksha-m20@users.noreply.github.com>
2022-12-06 17:40:09 +0530

    Add files via upload

---
+++ Quotient_remainder.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int Divident,Divisor,Quotient,Remainder;
+
+    printf("Enter divident:\n");
+    scanf("%d",&Divident);
+
+    printf("Enter divisor:\n");
+    scanf("%d",&Divisor);
+
+    Quotient = Divident / Divisor;
+
+    Remainder = Divident % Divisor;
+
+    printf("Quotient = %d\n Remainder = %d\n",Quotient,Remainder);
+
+    return 0;
+}
