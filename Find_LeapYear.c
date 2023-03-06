d786052b7d5d2486253fd95c2f721814fbdd7a89
Aakanksha Malwade <aakanksha.m20@gmail.com>
2023-03-06 22:44:20 +0530

    Update Find_LeapYear.c

--- Find_LeapYear.c
+++ Find_LeapYear.c
@@ -6,20 +6,18 @@ int main()
    printf("Enter a year: ");
    scanf("%d", &year);
 
-   // leap year if perfectly divisible by 400
+  // leap year if perfectly divisible by 400
    if (year % 400 == 0) 
    {
       printf("%d is a leap year.", year);
    }
    // not a leap year if divisible by 100
-   // but not divisible by 400
-   else if (year % 100 == 0) 
+   else if (year % 100 == 0)   // but if not divisible by 400 this step runs
    {
       printf("%d is not a leap year.", year);
    }
-   // leap year if not divisible by 100
-   // but divisible by 4
-   else if (year % 4 == 0) 
+   // runs this step if not divisible by 100
+   else if (year % 4 == 0)     // but leap year divisible by 4
    {
       printf("%d is a leap year.", year);
    }
